38d1ba71de2d641b22a3a22a00e4f127bbe2822e
mubarakmampuram <mubarak@neoito.com>
2022-10-26 16:09:16 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { AppDashboardComponent } from './app-dashboard/app-dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DisplayPostComponent } from './display-post/display-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    AppDashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProfileComponent,
+    DisplayPostComponent
   ],
   imports: [
     BrowserModule,
